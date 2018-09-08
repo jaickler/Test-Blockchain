270cad58f084b8c3f08be424f9bce30f9ebd9310
Jacob Allen Ickler <jaickler@comcast.net>
2018-09-08 19:38:29 -0400

    Added sha256 files for hashing function and added the Block.cpp file.

---
+++ Test-Blockchain/Test-Blockchain/Block.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "Block.h"
+#include "sha256.h"
+#include <strstream>
+#include <vector>
+
+SHA256 sha256;
+Block::Block(uint32_t nIndexIn, const string &sDataIn) : _nIndex(nIndexIn), _sData(sDataIn) {
+	_nNonce = -1;
+	_tTime = time(nullptr);
+}
+
+string Block::GetHash() {
+	return(_sHash);
+}
+
+void Block::MineBlock(uint32_t nDifficulty) {
+	char *cstr;
+	cstr = new char [nDifficulty + 1];
+	for (uint32_t i = 0; i < nDifficulty; i++) {
+		cstr[i] = '0';
+	}
+	cstr[nDifficulty] = '\0';
+
+	string str(cstr);
+
+	do {
+		_nNonce++;
+		_sHash = _CalculateHash();
+	} while (_sHash.substr(0, nDifficulty) != str);
+
+	cout << "Block mined: " << _sHash << endl;
+}
+
+inline string Block::_CalculateHash() const {
+	strstream SS;
+	SS << _nIndex << _tTime << _sData << _nNonce << sPrevHash;
+
+	return sha256(SS.str());
+
+}
