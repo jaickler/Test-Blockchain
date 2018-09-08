0a57f352122e3ff564d482047a7e15dcb7d99680
Jacob Allen Ickler <jaickler@comcast.net>
2018-09-08 19:04:17 -0400

    Finished defining the Block class in the header file.

--- Test-Blockchain/Test-Blockchain/Block.h
+++ Test-Blockchain/Test-Blockchain/Block.h
@@ -1,5 +1,15 @@
 #pragma once
+#include <cstdint>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Creates the definition for the Block class.
 class Block {
 public:
 
+	string sPrevHash;
+
+	Block()
 };
